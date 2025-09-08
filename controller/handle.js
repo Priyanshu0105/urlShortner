efaed42e7d423aa8dafacc0f647caa5024a667b7
Priyanshu <priyanshu41619@gmail.com>
2025-09-09 00:24:58 +0530

    controller

---
+++ controller/handle.js
@@ -0,0 +1,27 @@
+const {nanoid} = require("nanoid");
+const URL = require("../model/model")
+async function handleGenerateShortUrl(req ,res) {
+    const shortID = nanoid(8);
+    const body = req.body;
+    if(!body.url){
+        return res.status(400).json({msg: "url is required"})
+    }
+    await URL.create({
+        shortId: shortID,
+        redirectUrl: body.url,
+        visitHistory: []
+    });
+    return res.json({id : shortID});
+}
+async function handleGetAnalytics(req , res){
+    const shortId = req.params.shortId;
+    const result = await URL.findOne({shortId})
+    return res.json({
+        totalClicks: result.visitHistory.length,
+        analytics: result.visitHistory,
+    })
+}
+module.exports = {
+    handleGenerateShortUrl,
+    handleGetAnalytics,
+}
