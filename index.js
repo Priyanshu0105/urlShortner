ffbdbbe0079e71a8ad321dc624337709aa699ccb
Priyanshu <priyanshu41619@gmail.com>
2025-09-20 22:42:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express")
+const path = require("path")
 const app = express();
 const {connectMongoDb} = require("./connection")
+const staticRoute = require("./routes/staticRouter")
 const urlRouter = require("./routes/routes")
 const URL = require("./model/model")
 const PORT = 8001;
@@ -8,12 +10,20 @@ const PORT = 8001;
 connectMongoDb("mongodb://127.0.0.1:27017/urlShortner")
 .then(()=> console.log("server started"))
 
-
+app.set("view engine" , "ejs")
+app.set("views" , path.resolve("./views"))
 
 app.use(express.json());
-
+app.use(express.urlencoded({extended:false}))
+app.use("/" , staticRoute)
+app.get("/test" , async(req,res)=>{
+    const allUrls = await URL.find({});
+    return res.render("home" ,{
+        urls:allUrls,
+    });
+})
 app.use("/url" , urlRouter)
-app.get("/:shortId" , async (req , res) =>{
+app.get("/url/:shortId" , async (req , res) =>{
     const shortId = req.params.shortId;
     const entry = await URL.findOneAndUpdate({
         shortId
