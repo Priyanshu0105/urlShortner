02bb109b7e066f3b9775e6dffab832462e5143ff
Priyanshu <priyanshu41619@gmail.com>
2025-10-27 15:14:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ const staticRoute = require("./routes/staticRouter")
 const urlRouter = require("./routes/routes")
 const URL = require("./model/model")
 const PORT = 8001;
-
+const userRoute = require("./routes/user");
+const cookieParser = require("cookie-parser");
+const { restrictToLoggedinUserOnly ,checkAuth} = require("./middlewares/auth");
 connectMongoDb("mongodb://127.0.0.1:27017/urlShortner")
 .then(()=> console.log("server started"))
 
@@ -15,14 +17,16 @@ app.set("views" , path.resolve("./views"))
 
 app.use(express.json());
 app.use(express.urlencoded({extended:false}))
-app.use("/" , staticRoute)
+app.use(cookieParser());
+app.use("/" ,checkAuth, staticRoute)
+app.use("/user" , userRoute)
 app.get("/test" , async(req,res)=>{
     const allUrls = await URL.find({});
     return res.render("home" ,{
         urls:allUrls,
     });
 })
-app.use("/url" , urlRouter)
+app.use("/url" ,restrictToLoggedinUserOnly, urlRouter)
 app.get("/url/:shortId" , async (req , res) =>{
     const shortId = req.params.shortId;
     const entry = await URL.findOneAndUpdate({
